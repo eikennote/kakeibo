95629a7d5aa8599b780d9d041fd3982cdf488637
eiken <hideyoshi.noda@outlook.jp>
2024-06-16 12:17:49 +0900

    create method

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "/books", to: "books#index"
+  post "/books", to: "books#create"
   get "/books/new", to: "books#new", as: "new_book"
   get "/books/:id", to: "books#show", as: "book"
 
